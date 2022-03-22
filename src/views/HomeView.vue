bbcb4b9154e8d9c9272bd9ed4fc106c8fa5762ff
Filip <fkukic@student.unipu.hr>
2022-03-22 19:46:38 +0100

    Prvi uspjeh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <ul>
+    <li v-for="item in commits" v-bind:key="item.sha">
+    Commit {{ item.sha }}
+    </li>
+  </ul>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      commits: [],
+    };
+  },
+  async mounted() {
+    let rezultat = await fetch(
+      'https://api.github.com/repos/vuejs/vue/commits'
+    );
+    let podaci = await rezultat.json();
+
+    for (let item of podaci) {
+      console.log(item.sha);
+    }
+    this.commits = podaci;
+  },
+
   }
-}
 </script>
